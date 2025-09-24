40a39c0ded352c894c0745d0480b5e02b0211499
umuxt <umutyalcin802@gmail.com>
2025-09-25 02:31:07 +0300

    vps_deploy.sh ve vercel sorunu

--- quote-portal/src/i18n/index.js
+++ quote-portal/src/i18n/index.js
@@ -1,5 +1,5 @@
 // i18n dictionary and helpers (ES module)
 const { useState, useEffect, useMemo } = React
 
-export { dict, procLabel, materialLabel, finishLabel, tFor, statusLabel, useI18n } from './i18n.js'
+export { dict, procLabel, materialLabel, finishLabel, tFor, statusLabel, useI18n } from '../i18n.js'
 
